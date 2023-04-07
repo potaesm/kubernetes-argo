67663c66e0438c68d26b388034d026be3dd80239
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2023-04-08 01:34:43 +0530

    edit renew

--- renew-certificate.sh
+++ renew-certificate.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
 
 NAME_SPACE=argo
-
-# Delete the Certificate and its accompanying Secret
-# CERT_NAME=$(kubectl get certificates -n $NAME_SPACE -o name | cut -d'/' -f 2)
 CERT_NAME=le-cert
+SECRET_CERT_NAME=tls-cert-secret
+NAME_SPACE_CERTS_SECRET=le-cluster-issuer-key
+
 ORIGINAL_CERT_EXPIRY=$(kubectl get certificate -n $NAME_SPACE -o=jsonpath='{.items[0].status.notAfter}')
 echo "Certificate expires on $ORIGINAL_CERT_EXPIRY"
-
-# Use date -d if you're using non-BSD/MacOS
 ORIGINAL_CERT_EXPIRY_DATE=$(date -j -f "%F" $ORIGINAL_CERT_EXPIRY +"%s")
 
 kubectl delete certificate $CERT_NAME -n $NAME_SPACE
-
-# NAME_SPACE_CERTS_SECRET=$(kubectl get secrets -n $NAME_SPACE -o=jsonpath='{.items[1].metadata.name}')
-NAME_SPACE_CERTS_SECRET=le-cluster-issuer-key
+kubectl delete secret $SECRET_CERT_NAME -n $NAME_SPACE
 kubectl delete secret $NAME_SPACE_CERTS_SECRET -n $NAME_SPACE
 
-# Just for kicks
 CERT_MANAGER_POD_NAME=$(kubectl get pods -n cert-manager -o=jsonpath='{.items[0].metadata.name}')
 echo Pod name: $CERT_MANAGER_POD_NAME
 
-# Apply new certificate
 kubectl apply -f letsencrypt.yaml -n $NAME_SPACE
 
-# May take a bit of time for the certificate to generate. Putting in a pause.
 echo "Sleeping for 40 seconds to give the certificate time to generate"
 sleep 40
 
-# Get our new certificat's expiry date
-# kubectl describe certificates $CERT_NAME -n $NAME_SPACE
 NEW_CERT_EXPIRY=$(kubectl get certificate -n $NAME_SPACE -o=jsonpath='{.items[0].status.notAfter}')
 echo "New Certificate expires on $NEW_CERT_EXPIRY"
-# Use date -d if you're using non-BSD/MacOS
 NEW_CERT_EXPIRY_DATE=$(date -j -f "%F" $NEW_CERT_EXPIRY +"%s")
 
-# We want to make sure that the new certificate's expiry date is after our old certificate's expiry date
 if [ $NEW_CERT_EXPIRY_DATE -ge $ORIGINAL_CERT_EXPIRY_DATE ];
 then
     echo "Success! New certificate generated. New expiry date is $NEW_CERT_EXPIRY"
@@ -44,8 +33,6 @@ else
     exit 1
 fi
 
-# Let's make sure that the secret accompanying our certificate was also created
-SECRET_CERT_NAME=tls-cert-secret
 SECERT_NAME=$(kubectl get secrets -n $NAME_SPACE $SECRET_CERT_NAME -o name)
 if [ "$SECERT_NAME" == "secret/$SECRET_CERT_NAME" ];
 then
